050e20e165ebcb255bfca46a52fec37536acd34e
d_kujiraoka <d_kujiraoka@RH-1212006.local>
2015-11-20 13:22:25 +0900

    記事ページ修正

--- kujira-blog/single.php
+++ kujira-blog/single.php
@@ -1,34 +1,25 @@
-<?php get_header(); ?>	
-<!-- main -->
-<div id="content">
+<?php get_header(); ?>
 
-<div class="single_wrap">
-<?php
-if (have_posts()) : while (have_posts()) : the_post();
-//例えば下記のように、カテゴリによってdivのクラス名を変えたりできる
-//投稿がカテゴリ「1」に属していれば、CSSクラス"post-cat-one"のdivを、
-//それ以外なら、CSSクラス"post"のdivを表示
-      if (in_category('1')) { ?>
-      <div class="post-cat-one">
-      <?php } else{ ?>
-      <div class="post">
-      <?php } ?>
-//タイトルとパーマリンクを表示
-        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-//日時を表示
-        <small><?php the_time('Y年n月j日'); ?></small>
-//カテゴリをカンマ区切りで表示
-        <p class="postmetadata"><?php the_category(', '); ?></p>
-//投稿本文を表示
-        <div class="entry"><?php the_content(); ?></div>
-        </div> //最初のdiv（クラス名post-cat-oneかpost）を閉じ
-<?php endwhile; else: ?> //whileを終了し、投稿がないなら以下を表示
-      <p>記事が見つかりませんでした。</p>
-<?php endif; ?>
-</div>
+  <div id="content">
+    <div class="single_wrap u-cf">
+      <div class="single_inner">
+        
+        <?php if(have_posts()): while(have_posts()): the_post(); ?>
+        <p class="single_post_day"><?php echo get_the_date(); ?></p>
+        <h2 class="single_post_ttl"><?php the_title(); ?></h2>
+        <p class="single_post_category">カテゴリー: <?php the_category(', '); ?></p>
+        <hr>
 
+        <?php the_content(); ?>
+        <?php endwhile; endif; ?>
+      </div>
 
-	
-</div>
-		
+<?php get_sidebar(); ?>
+
+      <div id="page-top" class="go_top_btn">
+        <a href=""><img src="<?php echo get_template_directory_uri(); ?>/images/go_top.png" alt=""></a>
+      </div>
+    </div>
+  </div>
+
 <?php get_footer(); ?>
