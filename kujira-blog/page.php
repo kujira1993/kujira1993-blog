dd3cdcb5c9188d19a32dfb2d65e0e3617413d3dc
d_kujiraoka <d_kujiraoka@RH-1212006.local>
2015-11-24 19:15:38 +0900

    html

--- kujira-blog/page.php
+++ kujira-blog/page.php
@@ -1,14 +1,37 @@
-<?php get_header();?>
-
 <?php
-if (have_posts()) : 
-  while (have_posts()) :
-    the_post();
-    get_template_part('');
-  endwhile;
-endif;
+/*
+Template Name: about
+*/
 ?>
 
-<?php get_sidebar();?>
+<?php get_header(); ?>
 
-<?php get_footer();?>
+<div id="content" class="widecolumn">
+
+ <?php if (have_posts()) : while (have_posts()) : the_post();?>
+ <div class="post">
+  <h2 id="post-<?php the_ID(); ?>"><?php the_title();?></h2>
+  <div class="entrytext">
+   <?php the_content('<p class="serif">このページの続きを読む &raquo;</p>'); ?>
+  </div>
+ </div>
+ <?php endwhile; endif; ?>
+ <?php edit_post_link('この記事を編集', '<p>', '</p>'); ?>
+
+</div>
+<div id="main">
+
+<?php include (TEMPLATEPATH . '/searchform.php'); ?>
+
+<h2>月別アーカイブ</h2>
+  <ul>
+    <?php wp_get_archives('type=monthly'); ?>
+  </ul>
+
+<h2>カテゴリー別アーカイブ</h2>
+  <ul>
+     <?php wp_list_cats(); ?>
+  </ul>
+
+</div>
+<?php get_footer(); ?>
